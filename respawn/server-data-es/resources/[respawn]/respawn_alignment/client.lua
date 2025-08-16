9c884313fdce431ff4e0560b7ccadbde459543f7
alkaloRespawn <alkalo@respawncommunity.com>
2025-08-16 15:26:25 +0200

    remove old server and add images new items

--- respawn/server-data-es/resources/[respawn]/respawn_alignment/client.lua
+++
@@ -1,4 +0,0 @@
--- Placeholder de cliente (HUD futuro). Por ahora, sin UI.
-RegisterNetEvent('respawn:alignment:debug', function(state)
-  print('[Respawn Alignment] STATE', json.encode(state))
-end)
